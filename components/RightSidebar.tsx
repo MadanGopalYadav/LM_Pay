64e1f6771b3234f735adca1ec15f6f2d49c9bbae
Madana Gopal Yadav <madan@zelarsoft.com>
2024-05-09 15:40:44 +0530

    implement PlaidLink and Dwolla

--- components/RightSidebar.tsx
+++ components/RightSidebar.tsx
@@ -14,7 +14,7 @@ const RightSidebar = ({ user, transactions, banks }: RightSidebarProps) => {
         <div className="profile">
           <div className="profile-img">
             <span className="text-5xl font-bold text-blue-500">
-              {user.name[0]}
+              {user?.name[0]}
             </span>
           </div>
 
@@ -40,7 +40,7 @@ const RightSidebar = ({ user, transactions, banks }: RightSidebarProps) => {
               <BankCard
                 key={banks[0].$id}
                 account={banks[0]}
-                userName={user.name}
+                userName={user?.name}
                 showBalance={false}
               />
             </div>
@@ -49,7 +49,7 @@ const RightSidebar = ({ user, transactions, banks }: RightSidebarProps) => {
                 <BankCard
                   key={banks[1].$id}
                   account={banks[1]}
-                  userName={user.name}
+                  userName={user?.name}
                   showBalance={false}
                 />
               </div>
